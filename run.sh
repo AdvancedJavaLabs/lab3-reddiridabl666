89cf21d2598a36b90006a4682754949b3f18c3f7
Leonid Fiksler <l.fiksler@vkteam.ru>
2025-12-16 11:36:59 +0300

    Added results file

--- run.sh
+++ run.sh
@@ -13,8 +13,6 @@ if [ $# -lt 2 ]; then
     REDUCERS=1
 fi
 
-./gradlew fatJar
-
 docker cp $INPUT $HADOOP_NAME:/tmp/
 
 docker exec $HADOOP_NAME hdfs dfs -put /tmp/$INPUT /$INPUT
@@ -24,10 +22,6 @@ docker cp app/build/libs/app-standalone.jar $HADOOP_NAME:/tmp/job.jar
 docker exec $HADOOP_NAME hdfs dfs -rm -r /out
 docker exec $HADOOP_NAME hdfs dfs -rm -r /out-raw
 
-docker exec $HADOOP_NAME hadoop jar /tmp/job.jar /$INPUT /out $REDUCERS
-
-# for i in {0..9}; do
-#     docker exec $HADOOP_NAME hdfs dfs -cat /out/part-r-0000$i
-# done
+docker exec $HADOOP_NAME hadoop jar /tmp/job.jar /$INPUT /out $REDUCERS 4096
 
 docker exec $HADOOP_NAME hdfs dfs -cat /out/part-r-00000
